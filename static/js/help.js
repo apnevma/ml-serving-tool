a5695b4fe4c40164abc6f1a11a67cafda44ffa34
apnevma <apnevma@netcompany.com>
2026-02-09 16:10:00 +0200

    removed old ui logic

--- static/js/help.js
+++
@@ -1,8 +0,0 @@
-    document.addEventListener('DOMContentLoaded', () => {
-      document.querySelectorAll('.flip-btn').forEach(btn => {
-        btn.addEventListener('click', e => {
-          const cardInner = e.target.closest('.flip-card').querySelector('.flip-card-inner');
-          cardInner.classList.toggle('flipped');
-        });
-      });
-    });
